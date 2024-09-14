4fbad06cdcfa2edc8183bfce4c85b10bc2eb80bf
eltonanit <elton.ani@gmail.com>
2024-09-14 21:08:58 +0200

    [UPD] Fix axios index.php

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
 
-
 ?>
 
 
@@ -9,9 +8,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP to do list </title>
+    
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+<script src="https://unpkg.com/vue@3/dist/vue.global.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/axios/0.27.2/axios.min.js" integrity="sha512-odNmoc1XJy5x1TMVMdC7EMs3IVdItLPlCeL5vSUPN2llYKMJ2eByTTAIiiuqLg+GdNr9hF6z81p27DArRFKT7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <title>Document</title>
 </head>
 <body>
-    
+    <div id="app"></div>
+    <script src="./js/script.js" type="text/javascript"></script>
 </body>
 </html>
